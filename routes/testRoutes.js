026a5837c421ab260a05a988ed1a4b91fe967bf5
Nisindu <oneceupon2002@gmail.com>
2026-03-12 09:13:13 +0530

    force redeploy

--- routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import Test from "../models/Test.js";
-import { createTest } from "../controllers/testController.js";
-
-const testRouter = express.Router();
-
-testRouter.post("/", createTest);
-
-export default testRouter;
