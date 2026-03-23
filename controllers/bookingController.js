cae51b0a1ccaba3ba3df23c794d5bb269c54f099
Nisindu <oneceupon2002@gmail.com>
2026-03-23 20:59:39 +0530

    payment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,6 +2,7 @@ import Booking from "../models/Booking.js";
 import Hotel from "../models/Hotel.js";
 import Room from "../models/Room.js";
 import axios from "axios";
+import Stripe from "stripe";
 
 const sendBookingConfirmationEmail = async (to, username, booking, roomData) => {
    try {
@@ -138,5 +139,45 @@ export const getHotelBookings = async (req, res) => {
 
    } catch (error) {
       res.json({success: false, message: "Failed to fetch bookings"});
+   }
+}
+
+export const stripePayment = async (req, res) => {
+   try {
+      const { bookingId } = req.body;
+
+      const booking = await Booking.findById(bookingId);
+      const roomData = await Room.findById(booking.room).populate("hotel");
+      const totalPrice = booking.totalPrice;
+      const { origin } = req.headers;
+
+      const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+      const line_items = [
+         {
+            price_data: {
+               currency: process.env.CURRENCY || "usd",
+               product_data: {
+                  name: roomData.hotel.name,
+               },
+               unit_amount: totalPrice * 100,
+            },
+            quantity: 1,
+         }
+      ]
+
+      const session = await stripeInstance.checkout.sessions.create({
+         line_items,
+         mode: "payment",
+         success_url: `${origin}/loader/my-bookings`,
+         cancel_url: `${origin}/my-bookings`,
+         metadata: {
+            bookingId,
+         }
+      })
+      res.json({success: true, url: session.url});
+
+   } catch (error) {
+      res.json({success: false, message: "Payement failed"});
    }
 }
